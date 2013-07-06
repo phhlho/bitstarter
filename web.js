0cd95c4dbcbdbc33e915147c2d2accde64ae6936
Ubuntu <ubuntu@ip-10-196-141-191.ec2.internal>
2013-07-06 03:55:22 +0000

    Updated web.js because they told me to - and I made a mistake last time

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var message = rs.readFileSync('index.html').toString();
+  var message = fs.readFileSync('index.html').toString();
   response.send(message);
 });
 
